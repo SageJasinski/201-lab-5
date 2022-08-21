56b08f6e5a7b9611eb7d49de5e7d3d4eaf8f85a6
Sage Jasinski <sagejasinski@gmail.com>
2022-08-21 12:43:45 -0700

    sumarray passed

--- js/app.js
+++ js/app.js
@@ -91,12 +91,17 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  function raysum(a){
+    return a[0] + a[1] + a[2];
+  }
+  let getsum = raysum(sumArr);
+  let ray = new Array(getsum, sumArr + ' was passed in as an array of numbers, and ' + getsum + ' is their sum.')
+  return ray;
 }
-
+// sumArray(testArray);
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
