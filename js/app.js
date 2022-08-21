87e05afeddeeaa665de8ae789abe9ecda06849fc
Sage Jasinski <sagejasinski@gmail.com>
2022-08-20 23:49:03 -0700

    sum one passed

--- js/app.js
+++ js/app.js
@@ -8,12 +8,16 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
 
+function sum(a, b) { //eslint-disable-line
+  let add = a + b;
+  let cat = ('The sum of ' + a + ' and ' + b + ' is ' + add + '.');
+  let ray = new Array(add, cat);
+  return ray;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -47,6 +51,7 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
+
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 
 }
