490d856b9500603664df06084e0bae4e457774fd
Sage Jasinski <sagejasinski@gmail.com>
2022-08-21 12:50:49 -0700

    multArry passed

--- js/app.js
+++ js/app.js
@@ -117,11 +117,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
-
+  function multray(a){
+    return a[0] * a[1] * a[2];
+  }
+  let cheat = multray(multArr);
+  let ray = new Array(cheat, 'The numbers ' + multArr + ' have a product of ' + cheat + '.');
+  return ray;
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
